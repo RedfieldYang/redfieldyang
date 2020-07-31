22c76fa7204c77a200273c2df3bc367d179725dd
RedfielYang <lcmaciel@outlook.com.br>
2020-07-31 09:32:35 -0300

    Implementacao de State no form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const [nomeDaCategoria, setNomeDaCategoria]= useState('Valor inicial');
   return(
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+        <form>
+          {/* State */}
+          <label>
+            Nome da Categoria:
+            <input 
+              type="text"
+              value= {nomeDaCategoria}
+              onChange={function funcaoHandle(infosDoEvento) {
+               setNomeDaCategoria(infosDoEvento.target.value);
+              }}
+            />
+          </label>
 
-      <form>
-
-        <label>
-          Nome da Categoria:
-          <input 
-            type="text"
-          />
-        </label>
-
-        <button>
-          Cadastrar
-        </button>
-      </form>
+          <button>
+            Cadastrar
+          </button>
+        </form>
 
 
       <Link to="/">
